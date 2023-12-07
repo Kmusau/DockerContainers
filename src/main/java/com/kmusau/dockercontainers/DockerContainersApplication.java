4de2d329e3b4889ffbc2832d7d393d46a844b455
kmusau <kevomusa6565@gmail.com>
2023-12-07 12:43:40 +0300

    Bootstrapped project

---
+++ src/main/java/com/kmusau/dockercontainers/DockerContainersApplication.java
@@ -0,0 +1,13 @@
+package com.kmusau.dockercontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerContainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmusau/dockercontainers/DockerContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmusau.dockercontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
